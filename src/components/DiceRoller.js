cac5c6093754b21d5d772c2aa83dbda1b7a8264c
László Czellahó <laszlo.czellaho@gmail.com>
2020-06-08 15:41:29 +0200

    Init commit

---
+++ src/components/DiceRoller.js
@@ -0,0 +1,26 @@
+import React, {useEffect, useState} from 'react';
+
+const DiceRoller = (props) => {
+  const [units, setUnits] = useState([]);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    fetch("http://localhost:8080/units")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setUnits(result);
+        },
+        (error) => {
+          setError(error);
+        }
+      )
+  }, []);
+  return (
+    <div>
+      <p>DiceRoller</p>
+      {console.log(units)}
+    </div>
+  );
+};
+
+export default DiceRoller;
